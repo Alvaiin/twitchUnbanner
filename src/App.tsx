73cfcab2086c6316ba0693933892126965c152ad
Alexis Vainberg <alexisv@epidataconsulting.com>
2023-04-17 22:50:37 -0300

    agrego tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css'
+import React from 'react'
 
-function App() {
-
+function App (): JSX.Element {
   return (
     <div className="App">
-      <h1>Banking wb</h1>
+      <h1>Banking web</h1>
+      <form>
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
+          <div>
+            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
+          </div>
+          <div>
+            <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
+            <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required />
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
